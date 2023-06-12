09ddaacf6cad6567bf45ec837be87225618f1cb3
Md Mijanur Rahman <mizan.csegub@gmail.com>
2023-06-12 20:40:25 +0600

    New update

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello{
 	public static void main(String[] arg){
 		for(int i=0; i<10; i++){
 			System.out.println("Hello World .."+i);
-			System.out.println("Git Changed Added");
+			System.out.println("Git new Commit");
 
 		}
 		
