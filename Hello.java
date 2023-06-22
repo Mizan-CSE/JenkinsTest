276382a7c0a0bb44501e87ace141544dea605816
Mijanur Rahman <mijanur.rahman@datasoft-bd.com>
2023-06-22 11:16:29 +0600

    Test

--- Hello.java
+++ Hello.java
@@ -3,7 +3,10 @@ public class Hello{
 		for(int i=0; i<10; i++){
 			System.out.println("Hello World .."+i);
 		}
-		System.out.println("Git new Commit");
+		for(int i=0; i<10; i++){
+			System.out.println("Git new Commit");
+		}
+		
 		
 	}
 }
